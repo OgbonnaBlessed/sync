602d376feb17107f438c5101b50a08af705834e0
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-06-13 15:59:58 +0100

    feat: add auth middware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface AuthRequest extends Request {
+  user?: any; // You can make this more specific based on your User model
+}
+
+export const protect = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Not authorized, token missing" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+};
